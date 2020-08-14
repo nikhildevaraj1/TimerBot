793a3527ae92e97ab923c3897dd7cd10d6cb1d31
unknown <Nikhil Devaraj>
2020-08-14 01:27:53 -0500

    adding a bunch of new embeds

--- main.js
+++ main.js
@@ -12,26 +12,61 @@ client.on('message', message =>{
 
 
     if(command === 'help'){
-        message.channel.send('text');
-    }
-    if(command === 'help1'){
-        const exampleEmbed = new Discord.MessageEmbed()
+        const helpEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Help commands!')
-            .setURL('https://educationisttutoring.org/')
             .setAuthor('Educationist Tutoring', 'https://i1.wp.com/educationisttutoring.org/wp-content/uploads/2020/07/cropped-educationistlogo-1.png?fit=105%2C120&ssl=1', 'https://educationisttutoring.org/')
             .setDescription('Here are some of our bot commands that you can use!')
             .addFields(
                 { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Our Social Medias:', value: '!socialmedia'},
+                { name: 'Tutor Help Commands:', value: '!tutorquestions'},
+                { name: 'Next meeting:', value: '!nextmeeting'},
+                { name: 'Resources:', value: '!tutorresources'},
+                { name: 'FAQ (Please use this before you ask us a question):', value: '!faq'},
             )
             .setTimestamp()
             .setFooter('Educationist Tutoring© ', 'https://i2.wp.com/educationisttutoring.org/wp-content/uploads/2020/07/White-educationist-logo-banner.png?strip=info&w=300&ssl=1');
     
-    
+        message.channel.send(helpEmbed);
 
-        message.channel.send(exampleEmbed);
+    } else if(command === 'socialmedia'){
+        const socialEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Social Media:')
+            .setURL('https://linktr.ee/Educationist')
+            .setDescription('Here are our social media platforms!')
+            .addFields(
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Instagram', value: 'https://www.instagram.com/educationisttutoring/'},
+                { name: 'Facebook', value: 'https://www.facebook.com/educationist.tutoring.7'},
+                { name: 'Tiktok', value: 'https://www.tiktok.com/@educationisttutoring?lang=en'},
+                { name: 'LinkedIn', value: 'https://www.linkedin.com/company/educationist-tutoring/'},
+                { name: 'Twitter', value: 'https://twitter.com/EducationistTu1'},
+                { name: 'Tiktok: Our viral video **(135k+ views, 22k+ likes)**', value: 'https://www.tiktok.com/@educationisttutoring/video/6853538595874032902?lang=en'},
+                { name: 'Extra Information', value: 'Another important thing to remember is that if have any suggestions or have ideas for our social media, please let Jysotna know (@j.a.05#7869). She is our social media director and will be able to talk to you guys about your ideas and future posts. '},
+            )
+            .setTimestamp()
+            .setFooter('Educationist Tutoring© ', 'https://i2.wp.com/educationisttutoring.org/wp-content/uploads/2020/07/White-educationist-logo-banner.png?strip=info&w=300&ssl=1');
+        message.channel.send(socialEmbed);
+    } else if(command === 'tutorquestions'){
+        const tutorcommandsEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Common Tutor Questions:')
+            .setDescription('Here are some common questions and answers that we have gotton from our tutors so far. If you have a question that is not on here, please feel free to ask it on **#questions**')
+            .addFields(
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Student No Response', value: 'If your student does not respond within 1 week, please email us with how long it has been since you have emailed your student, your student name, and student age. After this, we will give you guys more information of how to get back on our waiting list.'},
+                { name: 'Tutor Resources', value: 'Please use our **!resources** command to get more information about our tutor resources'},
+                { name: 'How to get content curated?', value: 'If you are a current tutor, you can DM/email one of the content curators that introduced themselves in the **#curator-introduction-and-information** channel. Before this we would always reccomend checking our drive and seeing if that topic is already made to minimize repeats. '},
+                { name: 'How can I upload content I made?', value: 'Please DM/email one of the owners that **you want to add content you have made, your name, email, and specific subject you want to upload it for.**'},
+                { name: 'Twitter', value: 'https://twitter.com/EducationistTu1'},
+                { name: 'Tiktok: Our viral video (135k+ views, 22k+ likes)', value: 'https://www.tiktok.com/@educationisttutoring/video/6853538595874032902?lang=en'},
+                { name: 'Extra Information', value: 'Another important thing to remember is that if have any suggestions or have ideas for our social media, please let Jysotna know (@j.a.05#7869). She is our social media director and will be able to talk to you guys about your ideas and future posts. '},
+            )
+            .setTimestamp()
+            .setFooter('Educationist Tutoring© ', 'https://i2.wp.com/educationisttutoring.org/wp-content/uploads/2020/07/White-educationist-logo-banner.png?strip=info&w=300&ssl=1');
+        message.channel.send(tutorcommandsEmbed);
     }
 
 })
@@ -40,11 +75,5 @@ client.once('ready', ()=> {
     console.log('EducationistBot is online!')
 })
 
-client.on('message', msg  =>{
-    if(msg.content === "When is our next meeting?"){
-        msg.reply("Our next meeting is currently being decided by the owners. It like be sometime during the end of the month. During this we will take about the current state of Educationist and get more feedback from tutors and content curators. Once again, this meeting isn't mandatory but we strongly suggest you guys to attend and give us suggestions.")
-    }
-})
 
-
-client.login('')
+client.login('insert token')
